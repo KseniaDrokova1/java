319e608bc5c14e0f1419159cff2e107292baf9be
KseniaDrokova1 <149475475+KseniaDrokova1@users.noreply.github.com>
2023-10-31 14:34:54 +0300

    Add files via upload

---
+++ Zadacha_1_2_4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean varboolean = true;
+        byte varbyte = 1;
+        short varshort = 1000;
+        int varint = 100000;
+        long varlong = 1000L;
+        double vardouble = 3.45;
+        float varvloat = 1.31F;
+        char varchar = 1;
+    }
+
+}
