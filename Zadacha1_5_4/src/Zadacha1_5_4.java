47e1dcc547083b9195cff739dc49695d86f800c2
KseniaDrokova1 <149475475+KseniaDrokova1@users.noreply.github.com>
2023-11-28 17:05:41 +0300

    Add files via upload

---
+++ Zadacha1_5_4/src/Zadacha1_5_4.java
@@ -0,0 +1,43 @@
+
+public class Zadacha1_5_4 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 5, 8, 10, 11};
+        int start = 4;
+        int end = 12;
+        int[] arr = getSubArrayBetween(numbers, start, end);
+        int lastIndex = arr.length - 1;
+        int lastElement = arr[lastIndex];
+        System.out.print("[");
+        for (int j = 0; j < arr.length; j++) {
+            if (j == lastIndex) {
+                System.out.print(arr[j] + "]");
+            } else {
+                System.out.print(arr[j] + ",");
+            }
+        }
+
+
+    }
+
+    public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
+
+        int number = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= start && numbers[i] <= end) {
+                number++;
+            }
+        }
+
+        int number1 = 0;
+        int finalArray[] = new int[number];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= start && numbers[i] <= end) {
+                finalArray[number1] = numbers[i];
+                number1++;
+            }
+
+        }
+        return finalArray;
+    }
+}
