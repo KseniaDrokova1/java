a5ee5d604bdeb32f274b7ac1878878be36b276a1
KseniaDrokova1 <149475475+KseniaDrokova1@users.noreply.github.com>
2023-12-20 17:01:40 +0300

    Add files via upload

---
+++ Zadacha2_1_9/src/ComplexNumber.java
@@ -0,0 +1,52 @@
+public class ComplexNumber {
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+
+        ComplexNumber that = (ComplexNumber) o;
+
+        if (Double.compare(that.re, re) != 0) return false;
+        if (Double.compare(that.im, im) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(re);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(im);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public double getIm() {
+        return im;
+
+
+
+    }
+    public static void main(String[] args) {
+
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
+        System.out.println(a.equals(b));
+        System.out.println(a.hashCode() + " " + a.hashCode());
+    }
+}
