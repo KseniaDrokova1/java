3bffe5b16414e0208a92c0f6751024f0a2a290ab
KseniaDrokova1 <149475475+KseniaDrokova1@users.noreply.github.com>
2024-01-11 16:57:26 +0300

    Delete Zadacha2_1_9 directory

--- Zadacha2_1_9/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ComplexNumber a = new ComplexNumber(1, 1);
-        ComplexNumber b = new ComplexNumber(1, 1);
-        System.out.println(a.equals(b));
-    }
-}
