ff3b78768ca841ddf78c5bc6acfda3f4da9012fa
KseniaDrokova1 <149475475+KseniaDrokova1@users.noreply.github.com>
2024-04-05 20:09:31 +0300

    Update Main.java

--- Zadacha5_2_13v2/src/Main.java
+++ Zadacha5_2_13v2/src/Main.java
@@ -1,24 +1,28 @@
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> plenty1 = Set.of(1, 9, 3, 4);
-        Set<Integer> plenty2 = Set.of(1, 5, 3, 2);
+        Set<Integer> plenty1 = new HashSet<>();
+        Set<Integer> plenty2 = new HashSet<>();
+        plenty1.add(5);
+        plenty1.add(7);
+        plenty1.add(6);
+        plenty1.add(1);
+        plenty1.add(2);
+        plenty2.add(1);
+        plenty2.add(2);
+        plenty2.add(4);
+        plenty2.add(3);
+        plenty2.add(5);
         System.out.println(symmetricDifference(plenty1, plenty2));
-
     }
 
-
-    public static <T> Set<T> symmetricDifference(Set<? extends T> plenty1, Set<? extends T> plenty2) {
-
+    public static <T> Set<T> symmetricDifference(Set plenty1, Set plenty2) {
         Set<T> result = new HashSet<>(plenty1);
-        result.removeAll(plenty2);
-        Set<T> result1 = new HashSet<>(plenty2);
-        result1.removeAll(plenty1);
-        result.addAll(result1);
-
-        return result;
+        result.retainAll(plenty2);
+        plenty1.addAll(plenty2);
+        plenty1.removeAll(result);
+        return plenty1;
     }
 }
